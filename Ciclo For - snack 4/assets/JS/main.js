e3644aad7154ad8ef9deefa42f65b77ed1d42825
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-14 21:04:34 +0100

    worked on the evens and odds

--- Ciclo For - snack 4/assets/JS/main.js
+++ Ciclo For - snack 4/assets/JS/main.js
@@ -1,1 +1,10 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+for (let i = 0; i < 3; i++){
+    const Numbers = parseInt(prompt("inserire un numero;"))
+    if (i % 2 == 0){
+        console.log(Numbers)
+    } else {
+        console.log(Numbers+ 1)
+    }
+}
