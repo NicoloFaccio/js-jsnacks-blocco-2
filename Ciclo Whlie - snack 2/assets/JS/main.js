6bf63f117ec0896283cec1dcc77cd124ff5fe49c
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-15 20:16:16 +0100

    created the function and working on the cycle while

--- Ciclo Whlie - snack 2/assets/JS/main.js
+++ Ciclo Whlie - snack 2/assets/JS/main.js
@@ -1,2 +1,13 @@
 //genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il numero. Il programma poi deve far comparire un messaggio "il numero è troppo alto" oppure "il numero è troppo basso" nel momento in cui il numero detto dall'utente non è perfettamente uguale al numero estratto in modo casuale
 
+function numberRandom(max, min){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+let numRandom = numberRandom(1, 100)
+
+let i = 0
+
+while (i !== numRandom){
+    
+}
