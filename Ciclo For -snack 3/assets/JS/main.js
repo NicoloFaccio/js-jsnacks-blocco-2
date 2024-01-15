e37059a012de3c207657e3c745948cf2b2661a43
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-15 21:11:24 +0100

    created the cycle for and fixed the if

--- Ciclo For -snack 3/assets/JS/main.js
+++ Ciclo For -snack 3/assets/JS/main.js
@@ -1,1 +1,24 @@
 //Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+
+const num1 = [10, 20, 48, 50, 60];
+
+const num2 = [46, 26, 98, 29, 72, 33, 67, 22, 9, 10];
+
+let numMix;
+
+if(num1.length > num2.length) {
+    numMix = num2
+} else if (num1.length < num2.length) {
+    numMix = num1
+}
+
+function numberRandom(max, min){
+    return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+for(let i = 0; num1.length !== num2.length; i++){
+    let insertNumber = numberRandom(1,100)
+    numMix.push(insertNumber)
+}
+
+console.log(num1, num2)
