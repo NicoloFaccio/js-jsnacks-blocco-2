1fb0a711e5850c1c3a5c7d794310728d410fdd87
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-15 19:28:45 +0100

    Changed the i with Numbers in the if

--- Ciclo For - snack 4/assets/JS/main.js
+++ Ciclo For - snack 4/assets/JS/main.js
@@ -1,10 +1,14 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 for (let i = 0; i < 3; i++){
-    const Numbers = parseInt(prompt("inserire un numero;"))
-    if (i % 2 == 0){
+    
+    let Numbers = parseInt(prompt("inserire un numero;"))
+
+    if (Numbers % 2 == 0){
         console.log(Numbers)
+        document.writeln(`<p>Il numero è: ${Numbers}</p>`)
     } else {
         console.log(Numbers+ 1)
+        document.writeln(`<p>Il numero è: ${Numbers + 1}</p>`)
     }
 }
